b6fb7cff9c1ae82393410a78565e4d230a758340
SolomonTheCoder <solomonafutu1@gmail.com>
2025-09-03 13:14:16 +0000

    update

--- Backend/routes/translate.js
+++ Backend/routes/translate.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const axios = require('axios');
+const axios = require('axios')
 const Translation = require('../models/translation'); 
 const router = express.Router();
 
